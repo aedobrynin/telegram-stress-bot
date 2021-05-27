dbc8d0746c22db528bed2c0bb58f18319e38fd74
hashlib <43320720+hashlib@users.noreply.github.com>
2021-05-27 20:18:35 +0300

    Fixed stats storage

--- check_db.py
+++ check_db.py
@@ -12,16 +12,19 @@ class WordsTest(unittest.TestCase):
     def test_word_and_bad_variant_equality(self):
         session = Session()
         for word in session.query(Word).all():
-            self.assertEqual(word.word.lower(), word.bad_variant.lower())
+            self.assertEqual(word.word.lower(), word.bad_variant.lower(),
+                             msg=f"WORD ID: {word.id}")
         session.close()
 
     def test_only_one_stress_in_word(self):
         session = Session()
         for word in session.query(Word).all():
-            self.assertEqual(1, sum([char.isupper() for char in word.word]))
+            self.assertEqual(1, sum([char.isupper() for char in word.word]),
+                             msg=f"WORD ID: {word.id}")
             self.assertEqual(
                 1,
-                sum([char.isupper() for char in word.bad_variant])
+                sum([char.isupper() for char in word.bad_variant]),
+                msg=f"WORD ID: {word.id}",
             )
         session.close()
 
